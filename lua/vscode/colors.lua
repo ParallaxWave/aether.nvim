7bd258803d4d1d46d793867d640530a122f37632
Crypticwave <abhinavdatta38@gmail.com>
2021-11-21 17:35:13 +0530

    Remove lightbg

--- lua/vscode/colors.lua
+++ lua/vscode/colors.lua
@@ -2,7 +2,6 @@
 local generate = function ()
 
 	local colors = {}
-	if vim.g.vscode_style == "dark" then
 		colors = {
 			vscNone = 'NONE',
 			vscFront = '#ecf2f2',
@@ -56,60 +55,6 @@ local generate = function ()
 			vscPink = '#fc72ff',
 			vscUiBlue = '#3980cc',
 		}
-	else
-		colors = {
-			vscNone = 'NONE',
-			vscFront = '#343434',
-			vscBack = '#FFFFFF',
-
-			vscTabCurrent = '#FFFFFF',
-			vscTabOther = '#CECECE',
-			vscTabOutside = '#E8E8E8',
-
-			vscLeftDark = '#F3F3F3',
-			vscLeftMid = '#E5E5E5',
-			vscLeftLight = '#F3F3F3',
-
-			vscPopupFront = '#000000',
-			vscPopupBack = '#F3F3F3',
-			vscPopupHighlightBlue = '#D6EBFF',
-			vscPopupHighlightGray = '#767676',
-
-			vscSplitLight = '#EEEEEE',
-			vscSplitDark = '#DDDDDD',
-			vscSplitThumb = '#DFDFDF',
-
-			vscCursorDarkDark = '#E5EBF1',
-			vscCursorDark = '#6F6F6F',
-			vscCursorLight = '#767676',
-			vscSelection = '#ADD6FF',
-			vscLineNumber = '#098658',
-
-			vscDiffRedDark = '#FFCCCC',
-			vscDiffRedLight = '#FFA3A3',
-			vscDiffRedLightLight = '#FFCCCC',
-			vscDiffGreenDark = '#DBE6C2',
-			vscDiffGreenLight = '#EBF1DD',
-			vscSearchCurrent = '#A8AC94',
-			vscSearch = '#F8C9AB',
-
-			-- Syntax colors
-			vscGray = '#000000',
-			vscViolet = '#001080',
-			vscBlue = '#0000FF',
-			vscDarkBlue = '#007ACC',
-			vscLightBlue = '#0451A5',
-			vscGreen = '#008000',
-			vscBlueGreen = '#16825D',
-			vscLightGreen = '#098658',
-			vscRed = '#FF0000',
-			vscOrange = '#C72E0F',
-			vscLightRed = '#A31515',
-			vscYellowOrange = '#811F3F',
-			vscYellow = '#795E26',
-			vscPink = '#AF00DB',
-		}
-	end
 
 	return colors
 end
